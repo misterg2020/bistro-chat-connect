18e6d9ad62b4d33344cca553dcc31b3cff4ce977
misterg2020 <lemuelblowa2020@gmail.com>
2025-04-20 13:48:12 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Les valeurs proviennent de ton fichier .env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
